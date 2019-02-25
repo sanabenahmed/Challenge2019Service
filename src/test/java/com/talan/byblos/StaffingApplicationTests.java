eae0d0461c2d06cf40607c7c33465d9f49419b9c
sanabenahmed <sana.ben_ahmed@outlook.fr>
2019-02-25 10:17:12 +0100

    init

---
+++ src/test/java/com/talan/byblos/StaffingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.talan.byblos;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StaffingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
